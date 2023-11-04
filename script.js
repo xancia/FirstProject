1004b369eab691bae9b9aa19b67df9d04572cf8f
Xancia <cminhma@gmail.com>
2023-11-04 18:37:22 -0400

    removed unneeded files, refitted the game area

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 const canvas = document.querySelector('canvas')
+const ctr = canvas.getContext('2d')
+canvas.width = 304;
+canvas.height = 480;
 
+const image = new Image()
+image.src = './assets/Tile Set/zombieGameMap.png'
+image.onload = () => {
+    ctr.drawImage(image, 0, 0)
+}
